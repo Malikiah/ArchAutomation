efc7ac3d6480fe937d02d15b10fc47c66669d6db
Damian McCarthy <damianmmccarthy@protonmail.com>
2021-12-30 00:55:44 -0700

    update

--- archtautomation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-    bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchAutomation/1-setup.sh
-    source /mnt/root/ArchAutomation/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchAutomation/2-user.sh
-    arch-chroot /mnt /root/ArchAutomation/3-post-setup.sh
--- kderice-restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchAutomation/dotfiles/* $HOME/.config/
-pip install konsave
-konsave -i $HOME/ArchAutomation/kde.knsv
-sleep 1
-konsave -a kde
